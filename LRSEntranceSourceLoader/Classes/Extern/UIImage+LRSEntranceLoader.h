1ea911bd4e7f05b3cbcb76ffe382ffaf66be627b
刘sama <junchen.liu@jiamiantech.com>
2021-08-12 17:20:55 +0800

    代码优化

--- LRSEntranceSourceLoader/Classes/Extern/UIImage+LRSEntranceLoader.h
+++ LRSEntranceSourceLoader/Classes/Extern/UIImage+LRSEntranceLoader.h
@@ -11,7 +11,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIImage (LRSEntranceLoader)
 
-+ (instancetype)entrance_imageNamed:(NSString *)name;
++ (instancetype _Nullable)entrance_imageNamed:(NSString *)name;
 
 @end
 
