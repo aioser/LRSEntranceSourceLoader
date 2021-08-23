124dfd468c4e9c7a291f8156cf64907e8ef4e973
刘sama <junchen.liu@jiamiantech.com>
2021-08-23 10:29:10 +0800

    BUG修复

--- LRSEntranceSourceLoader/Classes/Loader/LRSGameOpeningInfoRes.m
+++ LRSEntranceSourceLoader/Classes/Loader/LRSGameOpeningInfoRes.m
@@ -18,8 +18,32 @@
     }
 }
 
+- (NSString<Ignore> *)pk {
+    return [@(self.gameRoomType) stringValue];
+}
+
 + (BOOL)propertyIsOptional:(NSString *)propertyName {
-    return [propertyName isEqualToString:@"roomType"];
+    return YES;
+}
+
++ (NSString *)modeNameWithType:(NSInteger)roomType name:(NSString *)name{
+    if (roomType == 0) {
+        return @"简单模式";
+    } else if (roomType == 1) {
+        return [NSString stringWithFormat:@"进阶(%@)模式", name];
+    } else if (roomType == 2 ) {
+        return [NSString stringWithFormat:@"娱乐(%@)模式", name];
+    } else {
+        BOOL hasModeSuffix = [name respondsToSelector:@selector(hasSuffix:)] && [name hasSuffix:@"模式"];
+        return [NSString stringWithFormat:@"%@%@",name,(hasModeSuffix ? @"" : @"模式")];
+    }
+}
+
+- (NSString<Ignore> *)modeName {
+    if (!_modeName) {
+        _modeName = [LRSGameOpeningInfoRes modeNameWithType:self.roomType name:self.name];
+    }
+    return _modeName;
 }
 
 @end
